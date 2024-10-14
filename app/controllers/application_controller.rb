ec350dd13bc8b6839174b7eb5a57b1606613cfa7
Korben Butterworth <korben250@gmail.com>
2024-10-14 03:48:22 +0100

    CCReg: Create Admin Role
    Use bundle exec rails g migration add_role_to_user
    to make migration and use it to add admin which can be true or false.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:email, :password, :password_confirmation, :admin)
+    end
+  end
 end
